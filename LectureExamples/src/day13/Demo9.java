314f51ae6601c6f04f2a00113cc06e85eba1ef8f
CCunningham0 <caleb.cunningham@revature.net>
2021-07-28 17:46:54 -0400

    Updated demo9

--- LectureExamples/src/day13/Demo9.java
+++ LectureExamples/src/day13/Demo9.java
@@ -5,10 +5,11 @@ import java.io.FileOutputStream;
 public class Demo9 {
 	/*
 	 * Try with resources
+	 * Automatically disposes resource after try catch block
 	 */
 	public static void main(String[] args) {
-		// Automatically disposes resource after try catch block
-		try (FileOutputStream stream = new FileOutputStream("data.txt")) { // Creates resource (object of FileOutputStream)
+		// Creates resource (object of FileOutputStream) *data.txt is placed in project directory 
+		try (FileOutputStream stream = new FileOutputStream("data.txt")) { 
 			System.out.println("Writing data");
 			
 			String data = "Hello World";
