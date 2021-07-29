ed2239788ae4d534aab882159222ca5eab0bf3ed
CCunningham0 <caleb.cunningham@revature.net>
2021-07-29 16:19:40 -0400

    Updated array list exercise

--- LectureExamples/src/day14/CollectionExample.java
+++ LectureExamples/src/day14/CollectionExample.java
@@ -1,6 +1,7 @@
 package day14;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CollectionExample {
 	public static void main(String[] args) {
@@ -22,16 +23,47 @@ public class CollectionExample {
 		
 		// Insert element at first position
 		colors.add(0, "Yellow");
+		System.out.println("\nAdded element at index 0: " + colors.get(0));
 		
 		// Retrieve element at specified position
 		int index = 0;
 		System.out.println("\nRetrieved element at index " + index + ": " + colors.get(index));
 		
 		// Update element with given element
-		//colors.remove(0);
-		String prevElement = colors.get(index);
+		String prevElement = colors.remove(0);
 		colors.add(0, "Purple");
-		System.out.println("\nElement at index " + index + " set to: " + colors.get(index));
+		System.out.println("\nUpdated element at index " + index + ": " + colors.get(index));
 		System.out.println("Previous element at index " + index + " was: " + prevElement);
+		
+		// Remove third element in array list
+		//System.out.println(colors);
+		prevElement = colors.remove(2);
+		System.out.println("\nRemoved third elment in array: " + prevElement);
+		System.out.println("Element is now: " + colors.get(2));
+		
+		// Search for element in array list
+		String givenElement = "Red";
+		int count = 0;
+		while (!colors.get(count).equals(givenElement)) {
+			count++;
+		}
+		System.out.println("\nFound element " + colors.get(count) + " at index " + count + "\n");
+		
+		// Sort array list
+		System.out.println("Sorting array list : " + colors);
+		Collections.sort(colors);
+		System.out.println("Sorted array list: " + colors + "\n");
+		
+		// Copy array list into another array list
+		ArrayList<String> colorShades = new ArrayList<>();
+		colorShades.add("Light");
+		colorShades.add("Medium");
+		colorShades.add("Dark");
+
+		colors.addAll(0, colorShades);
+		for (int i = 0; i < colorShades.size(); i++){
+			System.out.println("Element at index " + i + " is now: " + colors.get(i));
+		}
+		
 	}
 }
