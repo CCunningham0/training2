5457d97a298665fa2ada4eb91a9a8bb5cea2be12
Patronym <cunnical@mail.gvsu.edu>
2021-07-29 17:17:55 -0400

    Completed array list exercise

--- LectureExamples/src/day14/CollectionExample.java
+++ LectureExamples/src/day14/CollectionExample.java
@@ -3,6 +3,8 @@ package day14;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.colorchooser.ColorSelectionModel;
+
 public class CollectionExample {
 	public static void main(String[] args) {
 		/*
@@ -65,5 +67,56 @@ public class CollectionExample {
 			System.out.println("Element at index " + i + " is now: " + colors.get(i));
 		}
 		
+		// Shuffle elements in array list
+		System.out.println("\nShuffling array list : " + colors);
+		Collections.sort(colors);
+		System.out.println("Shuffled array list: " + colors + "\n");
+		
+		// Reverse elements in array list
+		System.out.println("Reversing array list : " + colors);
+		Collections.reverse(colors);
+		System.out.println("Reversed array list: " + colors + "\n");
+		
+		// Extract portion of array list
+		ArrayList<String> temp = new ArrayList<>();
+		int startIndex = 1;
+		int endIndex = 2;
+		
+		// add elements to temporary array list
+		while (startIndex <= endIndex) {
+			temp.add(colors.get(startIndex));
+			startIndex++;
+		}
+		
+		// iterate through array lists and remove any matching elements
+		// to temporary array (avoids dynamic array list sizing)
+		for (int j = 0; j < temp.size(); j++) {
+			for (int i = 0; i < colors.size(); i++) {	
+				if (colors.get(i).equals(temp.get(j))) {
+					colors.remove(i);
+					i = 0; // set back to beginning of array due to array resize after remove	
+				}
+			}
+		}
+
+		  
+		System.out.println("Extracted elements: " + temp);
+		System.out.println("New array list: " + colors);
+		
+		// Swap to elements in array list
+		String firstElement;
+		String lastElement;
+		System.out.println("\nSwapping first and last elements of: " + colors);
+		
+		firstElement = colors.get(0);
+		lastElement = colors.get(colors.size() - 1);
+		
+		// done after colors.get because array list is dynamic and indexes change after each remove()
+		colors.remove(0);
+		colors.remove(colors.size() - 1); // remove element at the last index
+		
+		colors.add(0, lastElement);		
+		colors.add(colors.size(), firstElement); // add element AFTER element in last index
+		System.out.println("Swapped array list: " + colors);
 	}
 }
